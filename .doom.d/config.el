1b8f395ca06ae726762baa519c73a9bde3bbb6ae
martin <martin.andrieux59@gmail.com>
2020-09-26 18:50:04 +0200

    fira code ligatures font

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -23,8 +23,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "Dejavu Sans Mono" :size 12))
-
+(setq doom-font (font-spec :family "Fira Code" :size 12))
+(use-package fira-code-mode)
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -10,6 +10,7 @@
 ;(package! some-package)
 (package! auctex)
 (package! flycheck-grammalecte)
+(package! fira-code-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
